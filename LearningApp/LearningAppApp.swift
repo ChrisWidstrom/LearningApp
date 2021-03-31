c2cd4bf3d6ca31039eae6f033af6a6949e8abd5d
Chris Widstrom <cwidstrom@me.com>
2021-03-30 20:18:33 -0400

    Initial Commit

---
+++ LearningApp/LearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppApp.swift
+//  LearningApp
+//
+//  Created by Christoffer Widstrom on 3/30/21.
+//
+
+import SwiftUI
+
+@main
+struct LearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
